c5e6042f3df084b9672768505db3e4e39b590786
U-BSD\357538 <357538@SHS-10D9C06CC.beaverton.k12.or.us>
2019-05-21 14:30:31 -0700

    OK I did the same amount now as i did in class

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
+
 
 //#include "graph.h"
 
@@ -17,6 +19,7 @@ int main(){
 	//void printMatrix();
 	void addEdge(Vertex*, Vertex*, bool);
 	Vertex* findVertex(char, Vertex**);
+	int vertexIndex(char, Vertex**);
 	char input[50];//cstring for input
 	bool running = true;//is the program still running
 	while (running){//as long as it is still running
@@ -52,42 +55,69 @@ int main(){
 				cout << endl;
 			}
 		}
-		else if (strcmp(input, "vertex") == 0){//if input is eaqual to quit 
-			char first[1];
-			char second[1];
+		else if (strcmp(input, "edge") == 0){//if input is eaqual to quit 
+			char first[50];
+			char second[50];
+			char bi[50];
 			cout << "First vertex: ";
-			cin.get(first, 1);//get users input
+			cin.get(first, 50);//get users input
 			cin.clear();
 			cin.ignore(10000, '\n'); 
 			cout << "Second vertex: ";
 			cin.get(second, 50);//get users input
 			cin.clear();
 			cin.ignore(10000, '\n');
+			cout << "Bidirectional (y or n): ";
+			cin.get(bi, 50);//get users input
+			cin.clear();
+			cin.ignore(10000, '\n');
 			cout << "Length: ";
 			cin.get(input, 50);//get users input
 			cin.clear();
 			cin.ignore(10000, '\n');
-			
+			int inter = atoi(input);//convers the char to an int
+			int y = vertexIndex(first[0], vertisies);
+			int x = vertexIndex(second[0], vertisies);
+			if(inter != NULL){
+				matrix[x][y] = inter;
+				if(bi[0] == 'y'){
+					matrix[y][x] = inter;
+				}
+			}
 		}
 		else if (strcmp(input, "delete") == 0){//if input is eaqual to quit 
 			cout << "Which vertex do you want to delete?: ";
 			cin.get(input, 50);//get users input
 			cin.clear();
 			cin.ignore(10000, '\n');
-			if(20 <= (int(*input) - 65)){
+			int temp = vertexIndex(input[0], vertisies);
+			if(temp != NULL){
 				for(int y = 0; numVert > y; y++){
-					matrix[int(*input)-65][y] = NULL;
+					matrix[temp][y] = NULL;
 				}
 				for(int x = 0; numVert > x; x++){
-					matrix[x][int(*input)-65] = NULL;
+					matrix[x][temp] = NULL;
 				}
-				vertisies[int(*input)-65] = NULL;
+				vertisies[temp] = NULL;
 			}
+			for(int x = temp + 1; numVert > x; x++){
+
+			}
+			numVert--;
 		}
 		else if (strcmp(input, "quit") == 0){//if input is eaqual to quit 
 			running = false;//set running to false
 		}
 	}
+}
+
+int vertexIndex(char v, Vertex** vertisies){
+	for(int i = 0; i < 20; i++){
+		if(v == vertisies[i]->label){
+			return i;
+		}
+	}	
+	return NULL;
 }
 
 Vertex* findVertex(char v, Vertex** vertisies){
